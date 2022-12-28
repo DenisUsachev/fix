55d35ac5d9dbf1ce22a8cb3a3db6e2925a70d1c9
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2022-12-28 23:30:59 +0500

    Add new commit

--- task55/Program.cs
+++ task55/Program.cs
@@ -1,2 +1,50 @@
 ﻿/*Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
 В случае, если это невозможно, программа должна вывести сообщение для пользователя*/
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] GenerateRandomMatrix(int min, int max, int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random r = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = r.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+int[,] Transpose(int[,] matrix)
+{
+    int m = matrix.GetLength(0);
+    int n = matrix.GetLength(1);
+    int[,] result = new int[n, m];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            result[i, j] = matrix[j, i];
+        }
+    }
+    return result;
+}
+
+int[,] randMatrix = GenerateRandomMatrix(0, 9, 4, 6);
+PrintMatrix(randMatrix);
+PrintMatrix(Transpose(randMatrix));
