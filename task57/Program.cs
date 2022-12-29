886ec5b2fd1ad2a26fa27fae2f9d1a618152635d
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2022-12-29 22:06:28 +0500

    add new commit

--- task57/Program.cs
+++ task57/Program.cs
@@ -1,3 +1,95 @@
 ﻿/*Задача 57: Составить частотный словарь элементов двумерного массива. 
-Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.*/
+Частотный словарь содержит информацию о том, 
+сколько раз встречается элемент входных данных.*/
 
+int m = GetNumber("Введите количество строк:");
+int n = GetNumber("Введите количество столбцов:");
+int[,] matrix = new int[m, n];
+FillArrayRandomNumbers(matrix);
+PrintArray(matrix);
+PrintArray(DeleteRowColoumn(matrix));
+
+
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result;
+}
+
+void FillArrayRandomNumbers(int[,] matrix)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+int[,] DeleteRowColoumn(int[,] matrix)
+{
+    int min = matrix[0, 0];
+    int x = 0;
+    int y = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] < min)
+            {
+                min = matrix[i,j];
+                x = i;
+                y = j;
+            }
+        }
+    }
+    int[,] newArr = new int[matrix.GetLength(0) - 1,matrix.GetLength(1) - 1];
+    int a = 0;
+    int b = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        if (i == x)
+        continue;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j == y)
+            continue;
+            newArr[a, b] = matrix[i, j];
+            b++;
+        }
+        b = 0;
+        a++;
+    }
+    return newArr;
+}
---
+++ task59/Program.cs
@@ -0,0 +1,80 @@
+﻿/*Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец,
+ на пересечении которых расположен наименьший элемент массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Наименьший элемент - 1, на выходе получим
+следующий массив:
+9 4 2
+2 2 6
+3 4 7
+*/
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] GenerateRandomMatrix(int min, int max, int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random r = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = r.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+int[,] DeleteMinRowColoumn(int[,] matrix)
+{
+    int minnum = matrix[0,0];
+    int x = 0;
+    int y = 0;
+
+    for (int i = 0; i < matrix.Getlength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i,j] < minnum)
+            {
+                minnum = matrix[i,j];
+                x = i;
+                y = j;
+            }
+        }
+    }
+    int[,] newArr = new int[matrix.GetLength(0) - 1,matrix.GetLength(1) - 1];
+    int a = 0;
+    int b = 0;
+
+    for (int i = 0; i < matrix.Getlength(0); i++)
+    {
+        if(i == x)
+            continue;
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(j == y)
+                continue;
+            new Arr[a, b] = matrix[i, j];
+            b++;
+        }
+        b = 0;
+        a++;
+    }
+}
+
